2c6eae3397c775c0603a7c1b461267b1a8663268
Victor Hugo Jimenez <vjimenezt@psl.com.co>
2018-10-25 09:05:14 -0500

    punto 5

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,7 +3,7 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/spec.js' ],
+  specs: [ '../test/google.spec.js' ],
   SELENIUM_PROMISE_MANAGER:false,
   capabilities: {
     browserName: 'chrome',
