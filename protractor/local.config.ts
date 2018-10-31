06054f2b7754cdcd9072bd0425dc726ab258ad25
Victor Hugo Jimenez Torres <vjimenezt@psl.com.co>
2018-10-31 07:12:38 -0500

    config

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -3,8 +3,12 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/spec.js' ],
+  specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER:false,
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
