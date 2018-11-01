269e4443c811315fd7fd04ea95267d6b45b60d2a
Victor Hugo Jimenez Torres <vjimenezt@psl.com.co>
2018-11-01 05:43:42 -0500

    punto 13

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -1,42 +1,34 @@
-import { $, browser } from 'protractor';
+import { $, browser, ExpectedConditions } from 'protractor';
 import { MenuContentPage } from '../src/page';
 
 describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
+  const EC = ExpectedConditions;
 
-  beforeEach(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
-  });
+  const menuContentPage: MenuContentPage = new MenuContentPage();
 
   it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
-    await(browser.sleep(3000));
     await menuContentPage.goToTShirtMenu();
-    await(browser.sleep(3000));
-    await $('#center_column a.button.ajax_add_to_cart_button.btn.btn-default').click();
-    await(browser.sleep(3000));
+
+    const btn = $('#center_column a.button.ajax_add_to_cart_button.btn.btn-default');
+    const isClickable = EC.elementToBeClickable(btn);
+    browser.wait(isClickable, 5000);
+    await btn.click();
+
     await $('[style*="display: block;"] .button-container > a').click();
-    await(browser.sleep(3000));
     await $('.cart_navigation span').click();
-    await(browser.sleep(3000));
 
     await $('#email').sendKeys('aperdomobo@gmail.com');
     await $('#passwd').sendKeys('WorkshopProtractor');
     await $('#SubmitLogin > span').click();
-    await(browser.sleep(3000));
 
     await $('#center_column > form > p > button > span').click();
-    await(browser.sleep(3000));
 
     await $('#cgv').click();
-    await(browser.sleep(3000));
 
     await $('#form > p > button > span').click();
-    await(browser.sleep(3000));
     await $('#HOOK_PAYMENT > div:nth-child(1) > div > p > a').click();
-    await(browser.sleep(3000));
     await $('#cart_navigation > button > span').click();
-    await(browser.sleep(3000));
 
     await expect($('#center_column > div > p > strong').getText())
       .toBe('Your order on My Store is complete.');
