eb9c6c724492fc8f096d30c25ac9bb5479b497ae
Victor Hugo Jimenez Torres <vjimenezt@psl.com.co>
2018-11-03 09:47:21 -0500

    all-locators

--- src/page/summary-step.page.ts
+++ src/page/summary-step.page.ts
@@ -1,13 +1,13 @@
 import { $, browser, ElementFinder, ExpectedConditions } from 'protractor';
 
-export class OrderSumaryPage {
+export class SumaryStepPage {
   private orderSumaryButton: ElementFinder;
 
   constructor () {
-    this.orderSumaryButton = $('#cart_navigation > button > span');
+    this.orderSumaryButton = $('.cart_navigation span');
   }
 
-  public async goToOrderSummary(): Promise<void> {
+  public async goToSummary(): Promise<void> {
     const EC = ExpectedConditions;
     const isClickable = EC.elementToBeClickable(this.orderSumaryButton);
     await browser.wait(isClickable, 5000);
--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -1,29 +1,31 @@
 import { $, browser } from 'protractor';
-import { AddressPage, MenuContentPage,
-  OrderSumaryPage, ProductAddModalPage, ShippingPage,  SignInPage } from '../src/page';
+import { AddressPage, BankPaymentPage, MenuContentPage, OrderSumaryPage,
+  SumaryStepPage, PaymentPage, ProductAddModalPage, ShippingPage,  SignInPage } from '../src/page';
 
 describe('Buy a t-shirt', () => {
 
   const menuContentPage: MenuContentPage = new MenuContentPage();
   const productAddModalPage: ProductAddModalPage = new ProductAddModalPage();
-  const orderSumaryPage: OrderSumaryPage = new OrderSumaryPage();
+  const sumaryStepPage: SumaryStepPage = new SumaryStepPage();
   const signInPage: SignInPage = new SignInPage();
   const addressPage: AddressPage = new AddressPage();
   const shippingPage: ShippingPage = new ShippingPage();
+  const paymentPage: PaymentPage = new PaymentPage();
+  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+  const orderSumaryStepPage: OrderSumaryPage = new OrderSumaryPage();
 
   it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
     await menuContentPage.goToTShirtMenu();
     await menuContentPage.addToCart();
     await productAddModalPage.addProduct();
-    await orderSumaryPage.goToSummary();
+    await sumaryStepPage.goToSummary();
     await signInPage.sigIn();
     await addressPage.goToAddress();
     await shippingPage.goToShipping();
-
-    await $('#form > p > button > span').click();
-    await $('#HOOK_PAYMENT > div:nth-child(1) > div > p > a').click();
-    await $('#cart_navigation > button > span').click();
+    await bankPaymentPage.goToBank();
+    await paymentPage.payment();
+    await orderSumaryStepPage.goToOrderSummary();
 
     await expect($('#center_column > div > p > strong').getText())
       .toBe('Your order on My Store is complete.');
