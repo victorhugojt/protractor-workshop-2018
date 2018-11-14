8bedf9c82b1e7d8553e72e4ba43c719cafc1d85b
Victor Hugo Jimenez <vjimenezt@psl.com.co>
2018-11-14 06:45:37 -0500

    punto 19

---
+++ src/page/personal-information.page.ts
@@ -0,0 +1,22 @@
+import { by, element } from 'protractor';
+
+export class PersonalInformationPage {
+  private firstNameField = element(by.name('firstname'));
+  private lastNameField = element(by.name('lastName'));
+  private sexField = element(by.className('sex'));
+  private experienceField = element(by.name('experience'));
+  private professionField = element(by.name('profession'));
+  private toolsField = element(by.name('tools'));
+  private continentField = element(by.name('continent'));
+
+  public async fillForm(formData: any): Promise<void> {
+    await this.firstNameField.sendKeys(formData.firstName);
+    await this.lastNameField.sendKeys(formData.lastName);
+    await this.sexField.sendKeys(formData.sex);
+    await this.experienceField.sendKeys(formData.experience);
+    await this.professionField.sendKeys(formData.profession[0]);
+    await this.toolsField.sendKeys(formData.tools[0]);
+    await this.continentField.sendKeys(formData.continent);
+  }
+
+}
